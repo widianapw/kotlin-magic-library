b9378c74d8fb407d86f743f16b1ae515981d281f
= <=>
2021-06-13 18:04:53 +0800

    initial commit

---
+++ magic/src/main/java/id/widianapw/magic/extensions.kt
@@ -0,0 +1,22 @@
+package id.widianapw.magic
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+object extensions {
+    fun EditText.onTextChanged(onTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                onTextChanged.invoke(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+}

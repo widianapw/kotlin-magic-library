af5e0472a22fdd8830879b009e942719af8fb0c0
= <=>
2021-06-13 18:20:28 +0800

    initial commit 12121

---
+++ magic/src/main/java/id/widianapw/magic/ToasterMessage.java
@@ -0,0 +1,11 @@
+package id.widianapw.magic;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+    }
+}

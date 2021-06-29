ae44daa199de37e1ab03d2b5a88a6a4f62e54fc8
= <=>
2021-06-29 14:49:46 +0800

    second try

--- magic/src/main/java/id/widianapw/magic/ToasterMessage.java
+++ magic/src/main/java/id/widianapw/magic/ToasterMessage.java
@@ -6,6 +6,5 @@ import android.widget.Toast;
 public class ToasterMessage {
     public static void s(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
-
     }
 }
